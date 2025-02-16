ca1eef0ad04d26a40aabaed9368dca2ce7186616
atlaca <ricardovilleda@yahoo.com>
2025-02-16 01:55:30 -0500

    Initial project setup with React, Tailwind CSS, and essential assets

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ZoomZoomyDemo from './ZoomZoomyDemo';
+
+function App() {
+  return (
+    <div className="app">
+      <ZoomZoomyDemo />
+    </div>
+  );
+}
+
+export default App;
